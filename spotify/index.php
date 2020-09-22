a1cc9bf64c917bd1f666c343331a185e859bd838
mrpandey1 <pandeynishant12345@gmail.com>
2020-09-22 13:01:37 +0530

    added volume controller

--- spotify/index.php
+++ spotify/index.php
@@ -66,7 +66,16 @@ if(isset($_SESSION['userLoggedIn'])){
                 </div>
             </div>
             <div id="nowPlayingRight">
-                
+                <div class="volumeBar">
+                    <button class="controlButton volume" title="Volume button">
+                        <img src="assets/images/icons/volume.png" alt="volume">
+                    </button>
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
