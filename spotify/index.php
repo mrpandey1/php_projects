dcb95039ca8e13d2e07336156a2ec24cdba5ce17
mrpandey1 <pandeynishant12345@gmail.com>
2020-09-22 12:33:58 +0530

    added progress bar

--- spotify/index.php
+++ spotify/index.php
@@ -18,7 +18,46 @@ if(isset($_SESSION['userLoggedIn'])){
 </head>
 <body>
     <div id='nowPlayingBarContainer'>
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+            
+            </div>
+            <div id="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class='controlButton shuffle' title="shuffle button">
+                            <img src="assets/images/icons/shuffle.png">
+                        </button>
+                        <button class='controlButton previous' title="previous button">
+                            <img src="assets/images/icons/previous.png">
+                        </button>
+                        <button class='controlButton play' title="play button">
+                            <img src="assets/images/icons/play.png">
+                        </button>
+                        <button class='controlButton next' title="next button">
+                            <img src="assets/images/icons/next.png">
+                        </button>
+                        <button class='controlButton repeat' title="repeat button">
+                            <img src="assets/images/icons/repeat.png">
+                        </button>
+                    </div>
 
+                    <div class="playbackBar">
+                        <span class='progressTime current'>0.00</span>
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        <span class='progressTime remaining'>0.00</span>
+                    </div>
+
+                </div>
+            </div>
+            <div id="nowPlayingRight">
+                
+            </div>
+        </div>
     </div>
 </body>
 </html>
