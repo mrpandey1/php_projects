d89344338e2a5682a866f0fb20bee79fa9aa3ae9
mrpandey1 <pandeynishant12345@gmail.com>
2020-09-23 12:36:51 +0530

    header and footer files

---
+++ spotify/includes/header.php
@@ -0,0 +1,25 @@
+<?php
+include('includes/config.php');
+
+//session_destroy
+
+if(isset($_SESSION['userLoggedIn'])){
+    $userLoggedIn=$_SESSION['userLoggedIn'];
+}else{
+    header('location:register.php');
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to spotify</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div id="mainContainer">
+        <div id="topContainer">
+            <?php include('includes/navBarContainer.php') ?>
+
+            <div id="mainViewContainer">
+                <div id="mainContent">
--- spotify/index.php
+++ spotify/index.php
@@ -1,29 +1,3 @@
-<?php
-include('includes/config.php');
 
-//session_destroy
-
-if(isset($_SESSION['userLoggedIn'])){
-    $userLoggedIn=$_SESSION['userLoggedIn'];
-}else{
-    header('location:register.php');
-}
-?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to spotify</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-    <div id="mainContainer">
-        <div id="topContainer">
-            <?php include('includes/navBarContainer.php') ?>
-        </div>
-
-        <?php include('includes/nowPlayingBar.php') ?>
-
-    </div>
-</body>
-</html>
+<?php include('includes/header.php');?>
+<?php include('includes/footer.php');?>
