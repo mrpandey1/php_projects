96356f5fd840d6c2b82c471374e898eefb31855d
mrpandey1 <pandeynishant12345@gmail.com>
2020-09-23 12:51:16 +0530

    outputting the album artwork images

--- spotify/index.php
+++ spotify/index.php
@@ -1,3 +1,19 @@
 
 <?php include('includes/header.php');?>
+
+<h1 class='pageHeadingBig'>You might also like this</h1>
+<div class="gridViewContainer">
+    <?php 
+        $albumQuery=mysqli_query($con,"SELECT * from albums ORDER BY RAND() LIMIT 10");
+        while ($row=mysqli_fetch_array($albumQuery)){
+            echo "<div class='gridViewItem'>
+                    <img src='".$row['artworkPath']."'>
+                    <div class='gridViewInfo'>"
+                    .$row['title']."
+                    </div>
+                </div>";
+        } 
+    ?>
+</div>
+
 <?php include('includes/footer.php');?>
