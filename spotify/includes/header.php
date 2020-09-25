b031c1a162412bf55a19a47d1507504130c9415a
mrpandey1 <pandeynishant12345@gmail.com>
2020-09-25 11:47:25 +0530

    first playlist of random songs

--- spotify/includes/header.php
+++ spotify/includes/header.php
@@ -1,46 +1,29 @@
 <?php
-include("includes/config.php");
-include("includes/classes/Artist.php");
-include("includes/classes/Album.php");
-include("includes/classes/Song.php");
+include('includes/config.php');
+include('includes/classes/Artist.php');
+include('includes/classes/Album.php');
+include('includes/classes/Song.php');
+//session_destroy
 
-//session_destroy(); LOGOUT
-
-if(isset($_SESSION['userLoggedIn'])) {
-	$userLoggedIn = $_SESSION['userLoggedIn'];
+if(isset($_SESSION['userLoggedIn'])){
+    $userLoggedIn=$_SESSION['userLoggedIn'];
+}else{
+    header('location:register.php');
 }
-else {
-	header("Location: register.php");
-}
-
 ?>
-
-<html>
+<html lang="en">
 <head>
-	<title>Welcome to Slotify!</title>
-
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-
-	<script src="assets/js/script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to spotify</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/script.js"></script>
 </head>
-
 <body>
 
+    <div id="mainContainer">
+        <div id="topContainer">
+            <?php include('includes/navBarContainer.php') ?>
 
-
-	<script>
-	var audioElement = new Audio();
-	audioElement.setTrack("assets/music/bensound-acousticbreeze.mp3");
-	audioElement.audio.play();
-
-	</script>
-
-	<div id="mainContainer">
-
-		<div id="topContainer">
-
-			<?php include("includes/navBarContainer.php"); ?>
-
-			<div id="mainViewContainer">
-
-				<div id="mainContent">
+            <div id="mainViewContainer">
+                <div id="mainContent">
--- spotify/includes/nowPlayingBar.php
+++ spotify/includes/nowPlayingBar.php
@@ -1,4 +1,14 @@
-
+<?php
+    $songQuery=mysqli_query($con,"SELECT id from Songs ORDER BY RAND() LIMIT 10");
+    $resultArray=array();
+    while ($row=mysqli_fetch_array($songQuery)){
+        array_push($resultArray,$row['id']);
+    }
+    $jsonArray=json_encode($resultArray);
+?>
+<script>
+    console.log(<?php echo $jsonArray ?>);
+</script>
 
 <div id='nowPlayingBarContainer'>
             <div id="nowPlayingBar">
