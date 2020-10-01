b686cd2215c601915a82ed3ac6c40c6480f517ce
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-01 15:18:30 +0530

    album section in artist page

--- spotify/artist.php
+++ spotify/artist.php
@@ -22,6 +22,7 @@ $artist=new Artist($con,$artistId);
 </div>
 
 <div class="tracklistContainer borderBottom">
+<h2>Songs</h2>
 	<ul class="tracklist">
 		
 		<?php
@@ -59,4 +60,21 @@ $artist=new Artist($con,$artistId);
 		</script>
 
 	</ul>
+</div>
+
+<div class="gridViewContainer">
+<h2>Albums</h2>
+	<?php
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE artist='$artistId' LIMIT 10");
+		while($row = mysqli_fetch_array($albumQuery)) {
+			echo "<div class='gridViewItem'>
+					<span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+						<img src='" . $row['artworkPath'] . "'>
+						<div class='gridViewInfo'>"
+							. $row['title'] .
+						"</div>
+					</span>
+				</div>";
+		}
+	?> 
 </div>
