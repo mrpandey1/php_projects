7dc7b046c76c77f973daba9189a92d53796bf11c
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-01 15:10:40 +0530

    songs section in artist page

--- spotify/artist.php
+++ spotify/artist.php
@@ -10,7 +10,7 @@ else {
 $artist=new Artist($con,$artistId);
 ?>
 
-<div class="entityInfo">
+<div class="entityInfo borderBottom">
     <div class="centerSection">
         <div class="artistInfo">
             <h1 class="artistName"><?php echo $artist->getName(); ?></h1>
@@ -19,4 +19,44 @@ $artist=new Artist($con,$artistId);
             </div>
         </div>
     </div>
+</div>
+
+<div class="tracklistContainer borderBottom">
+	<ul class="tracklist">
+		
+		<?php
+        $songIdArray = $artist->getSongIds();
+		$i = 1;
+		foreach($songIdArray as $songId) {
+            if ($i>5){
+                break;
+            }
+			$albumSong = new Song($con, $songId);
+			$albumArtist = $albumSong->getArtist();
+			echo "<li class='tracklistRow'>
+					<div class='trackCount'>
+						<img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
+						<span class='trackNumber'>$i</span>
+					</div>
+					<div class='trackInfo'>
+						<span class='trackName'>" . $albumSong->getTitle() . "</span>
+						<span class='artistName'>" . $albumArtist->getName() . "</span>
+					</div>
+					<div class='trackOptions'>
+						<img class='optionsButton' src='assets/images/icons/more.png'>
+					</div>
+					<div class='trackDuration'>
+						<span class='duration'>" . $albumSong->getDuration() . "</span>
+					</div>
+				</li>";
+			$i = $i + 1;
+		}
+		?>
+
+		<script>
+			var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+			tempPlaylist = JSON.parse(tempSongIds);
+		</script>
+
+	</ul>
 </div>
