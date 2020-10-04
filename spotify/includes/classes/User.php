6f9624df13949fbefa94c0ac3021e5c6eabd3fd4
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-04 13:27:27 +0530

    added User class

---
+++ spotify/includes/classes/User.php
@@ -0,0 +1,11 @@
+<?php
+	class User{
+
+		private $con;
+        private $username;
+		public function __construct($con,$username) {
+            $this->con = $con;
+            $this->username=$username;
+		}
+	}
+?>
--- spotify/includes/includedFiles.php
+++ spotify/includes/includedFiles.php
@@ -2,9 +2,16 @@
 
 if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
 	include("includes/config.php");
+	include("includes/classes/User.php");
 	include("includes/classes/Artist.php");
 	include("includes/classes/Album.php");
 	include("includes/classes/Song.php");
+	if(isset($_GET['userLoggedIn'])){
+		$userLoggedIn=new User($con,$_GET['userLoggedIn']);
+	}else{
+		echo "username variable not passed ";
+		exit();
+	}
 }
 else {
 	include("includes/header.php");
