34763d44eb29834ac9538a8fe275b494dfb8a9f3
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-06 13:47:19 +0530

    Removing songs from playlist ajax call

--- spotify/assets/js/script.js
+++ spotify/assets/js/script.js
@@ -54,7 +54,17 @@ function openPage(url) {
 	$("body").scrollTop(0);
 	history.pushState(null, null, url);
 }
-
+function removeFromPlaylist(button,playlistId){
+	var songId = $(button).prevAll(".songId").val();
+	$.post("includes/handlers/ajax/removeFromPlaylist.php", { playlistId: playlistId,songId:songId })
+	.done(function(error) {
+		if(error != "") {
+			alert(error);
+			return;
+		}
+		openPage("playlist.php?id="+playlistId);
+	});
+}
 function createPlaylist() {
 	console.log(userLoggedIn);
 	var popup = prompt("Please enter the name of your playlist");
