dcd5ef59c80156192a921233e74c7e92af10d99a
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-06 19:27:37 +0530

    user details form

--- spotify/includes/classes/User.php
+++ spotify/includes/classes/User.php
@@ -10,6 +10,11 @@
 		public function getUsername(){
 			return $this->username;
 		}
+		public function getEmail(){
+			$query=mysqli_query($this->con,"SELECT email from users where username='$this->username'");
+			$row=mysqli_fetch_array($query);
+			return $row['email'];
+		}
 		public function getName(){
 			$query=mysqli_query($this->con,"SELECT concat(firstname,' ',lastname) as 'name' from users where username='$this->username'");
 			$row=mysqli_fetch_array($query);
