983b7cb3d438f9a740a924631f0fb1776d0e61da
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-06 13:57:05 +0530

    added settings page

--- spotify/includes/classes/User.php
+++ spotify/includes/classes/User.php
@@ -10,5 +10,10 @@
 		public function getUsername(){
 			return $this->username;
 		}
+		public function getName(){
+			$query=mysqli_query($this->con,"SELECT concat(firstname,' ',lastname) as 'name' from users where username='$this->username'");
+			$row=mysqli_fetch_array($query);
+			return $row['name'];
+		}
 	}
 ?>
---
+++ spotify/settings.php
@@ -0,0 +1,14 @@
+<?php
+include('includes/includedFiles.php');
+?>
+<div class="entityInfo">
+    <div class="centerSection">
+        <div class="userInfo">
+            <h1><?php echo $userLoggedIn->getName(); ?></h1>
+        </div>
+    </div>
+    <div class="buttonItems">
+        <button class="button">User Details</button>
+        <button class="button">Logout</button>
+    </div>
+</div>
